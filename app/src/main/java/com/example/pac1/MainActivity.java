5170fac063250057cf02a8af470b431ab417b472
Laia Luna <aialanul.000@gmail.com>
2020-02-27 19:10:07 +0100

    Fase 1 del tutorial

--- app/src/main/java/com/example/pac1/MainActivity.java
+++ app/src/main/java/com/example/pac1/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.pac1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView ambleBold, ambleLight, ambleRegular, openSansItalic, openSansRegular;
+    Button btn;
+
+    private String A_BOLD= "Amble-Bold.ttf";
+    private String A_LIGHT="Amble-Light.ttf";
+    private String A_REGULAR= "Amble-Regular.ttf";
+    private String O_ITALIC= "OpenSans-Italic.ttf";
+    private String O_REGULAR="OpenSans-Regular.ttf";
+    private String P_REGULAR="Pacifico.ttf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_custom_fonts);
+
+        InitControles();
+
+        // Añadimos estilos
+        ambleBold.setTypeface(Typeface.createFromAsset(getAssets(), A_BOLD));
+        ambleLight.setTypeface(Typeface.createFromAsset(getAssets(), A_LIGHT));
+        ambleRegular.setTypeface(Typeface.createFromAsset(getAssets(), A_REGULAR));
+        openSansRegular.setTypeface(Typeface.createFromAsset(getAssets(), O_REGULAR));
+        openSansItalic.setTypeface(Typeface.createFromAsset(getAssets(), O_ITALIC));
+        btn.setTypeface(Typeface.createFromAsset(getAssets(), P_REGULAR));
+    }
+
+    // Inicializamos los controles de la pantalla
+    private void InitControles(){
+        ambleBold=(TextView)findViewById(R.id.ambleBold);
+        ambleLight=(TextView)findViewById(R.id.ambleLight);
+        ambleRegular=(TextView)findViewById(R.id.ambleRegular);
+        openSansRegular=(TextView)findViewById(R.id.opRegular);
+        openSansItalic=(TextView)findViewById(R.id.opItalic);
+        btn=(Button)findViewById(R.id.pacifico);
     }
 }
